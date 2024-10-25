c76ea4e3707222f418f7dca8f9124ddd6b4622d4
bkkurandwad <bhargavkurandwad@gmail.com>
2024-10-26 01:47:57 +0530

    First Commit

---
+++ src/main/java/com/majorproject/CareerforIT/CareerforItApplication.java
@@ -0,0 +1,13 @@
+package com.majorproject.CareerforIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerforItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerforItApplication.class, args);
+	}
+
+}
